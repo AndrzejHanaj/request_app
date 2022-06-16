33a88ddf8e05ed6139e167c57129d16eaa3e5889
AndrzejHanaj <andrzej.hanaj@op.pl>
2022-06-16 13:50:18 +0200

    stany aplikacji

--- src/components/request-form/request-form.js
+++ src/components/request-form/request-form.js
@@ -4,14 +4,36 @@ import "./style.css";
 
 
 const RequestForm = () => {
-    const state= useState(0)
+
+    const [personInfo, setPersonInfo] = useState({
+        name: "",
+        secondName: "",
+        surname: "",
+        dateOfBirth: "",
+        placeOfBirth: "",
+        personalId: "",
+        nationality: "",
+
+
+    });
+
+    const handleChange = (event) => {
+        setPersonInfo({ ...personInfo, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        // prevents the submit button from refreshing the page
+        event.preventDefault();
+        console.log(personInfo);
+    };
+
     return(
 
 <div>
 
     <p className= "wypij"> W celu złożenia wniosku o paszport proszę wypełnić wszystkie pola </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
 
             {/*<div className="form-check">*/}
             {/*    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>*/}
@@ -29,46 +51,88 @@ const RequestForm = () => {
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Imię</label>
                 <div className="col-sm-10">
-                    <input type="text" className="form-control" name='name' placeholder="Wpisz imię"/>
+                    <input type="text"
+                           className="form-control"
+                           name='name'
+                           placeholder="Wpisz imię"
+                           value={personInfo.name}
+                           onChange={handleChange}
+                    />
                 </div>
             </div>
 
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Drugie Imię</label>
                 <div className="col-sm-10">
-                    <input type="text" className="form-control" name='name' placeholder="Wpisz drugie imię"/>
+                    <input type="text"
+                           className="form-control"
+                           name='secondName'
+                           placeholder="Wpisz drugie imię"
+                           value={personInfo.secondName}
+                           onChange={handleChange}
+                    />
                 </div>
             </div>
 
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Nazwisko</label>
                 <div className="col-sm-10">
-                    <input type="text" className="form-control" name='name' placeholder="Wpisz Nazwisko"/>
+                    <input type="text"
+                           className="form-control"
+                           name='surname'
+                           placeholder="Wpisz Nazwisko"
+                           value={personInfo.surname}
+                           onChange={handleChange}
+                    />
                 </div>
             </div>
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Data urodzenia</label>
                 <div className="col-sm-10">
-                    <input type="date" className="form-control" name='name' placeholder="Wpisz Nazwisko"/>
+                    <input type="date"
+                           className="form-control"
+                           name='dateOfBirth'
+                           placeholder="Wpisz Nazwisko"
+                           value={personInfo.dateOfBirth}
+                           onChange={handleChange}
+                    />
                 </div>
             </div>
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Miejsce urodzenia</label>
                 <div className="col-sm-10">
-                    <input type="text" className="form-control" name='name' placeholder="Wpisz miejsce urodzenia"/>
+                    <input type="text"
+                           className="form-control"
+                           name='placeOfBirth'
+                           placeholder="Wpisz miejsce urodzenia"
+                           value={personInfo.placeOfBirth}
+                           onChange={handleChange}
+                    />
                 </div>
                 </div>
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Pesel</label>
                 <div className="col-sm-10">
-                    <input type="number" className="form-control" name='name' placeholder="Wpisz swój numer pesel"/>
+                    <input type="number"
+                           className="form-control"
+                           name='personalId'
+                           placeholder="Wpisz swój numer pesel"
+                           value={personInfo.personalId}
+                           onChange={handleChange}
+                    />
                 </div>
             </div>
 
             <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Narodowość</label>
                 <div className="col-sm-10">
-                    <input type="text" className="form-control" name='name' placeholder="Podaj swoją narodowość"/>
+                    <input type="text"
+                           className="form-control"
+                           name='nationality'
+                           placeholder="Podaj swoją narodowość"
+                           value={personInfo.nationality}
+                           onChange={handleChange}
+                    />
                 </div>
             </div>
 
@@ -109,7 +173,8 @@ const RequestForm = () => {
 
 </div>
 
-            <button>Wyślij formularz</button>
+            <button type="submit">Wyślij formularz
+            </button>
         </form>
 
 </div>
